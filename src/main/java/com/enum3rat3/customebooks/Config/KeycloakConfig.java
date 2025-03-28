cd58fbbea902eef58a7e07f36657d325db10c6ad
enum3rat3 <jaiminjadvani3728@gmail.com>
2025-03-28 22:35:21 +0530

    keycloak configuration

--- src/main/java/com/enum3rat3/customebooks/Config/KeycloakConfig.java
+++ src/main/java/com/enum3rat3/customebooks/Config/KeycloakConfig.java
@@ -1,39 +1,5 @@
 package com.enum3rat3.customebooks.Config;
 
-import jakarta.ws.rs.client.Client;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.keycloak.OAuth2Constants;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
-
 public class KeycloakConfig {
-    static Keycloak keycloak = null;
 
-    final static String serverUrl = "http://localhost:8082/";
-    public final static String realm = "enum3rat3";
-    final static String clientId = "custom-ebook";
-    final static String userName = "admin";
-    final static String password = "admin";
-
-    public KeycloakConfig() {
-    }
-
-    public static Keycloak getInstance(){
-        if(keycloak == null){
-
-            keycloak = KeycloakBuilder.builder()
-                    .serverUrl(serverUrl)
-                    .realm(realm)
-                    .grantType(OAuth2Constants.PASSWORD)
-                    .username(userName)
-                    .password(password)
-                    .clientId(clientId)
-                    .resteasyClient((Client) new ResteasyClientBuilder()
-                            .connectionPoolSize(10)
-                            .build()
-                    )
-                    .build();
-        }
-        return keycloak;
-    }
 }
